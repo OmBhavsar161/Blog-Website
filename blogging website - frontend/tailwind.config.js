91a94bd45ac8bbba5e24ce9dac7857b6981a59be
DollyAgrawal2901 <ashitaa38@gmail.com>
2024-10-14 21:02:43 +0530

    changes

--- blogging website - frontend/tailwind.config.js
+++ blogging website - frontend/tailwind.config.js
@@ -2,17 +2,6 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        
-        colors: {
-            'white': '#FFFFFF',
-            'black': '#242424',
-            'grey': '#F3F3F3',
-            'dark-grey': '#6B6B6B',
-            'red': '#FF4E4E',
-            'transparent': 'transparent',
-            'twitter': '#1DA1F2',
-            'purple': '#8B46FF'
-        },
 
         fontSize: {
             'sm': '12px',
